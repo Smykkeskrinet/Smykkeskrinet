d7eea406032d40fffce2f89fbe699dbaa4ac250e
juliewbech <jube0001@stud.kea.dk>
2024-12-13 20:54:40 +0100

    js

--- src/js/js.js
+++ src/js/js.js
@@ -1,13 +1,14 @@
-document.querySelectorAll(".dropdown > a").forEach((menuLink) => {
-  menuLink.addEventListener("click", (event) => {
-    event.preventDefault();
-    const dropdown = menuLink.nextElementSibling;
+const sortButton = document.getElementById("sort-toggle");
+const sortMenu = document.getElementById("sort-menu");
 
-    // Skift mellem at vise og skjule dropdown
-    if (dropdown.style.display === "block") {
-      dropdown.style.display = "none";
-    } else {
-      dropdown.style.display = "block";
-    }
-  });
+sortButton.addEventListener("click", () => {
+  // Toggle menuens synlighed
+  sortMenu.classList.toggle("active");
+});
+
+// Luk dropdown-menuen, hvis man klikker udenfor
+document.addEventListener("click", (event) => {
+  if (!sortButton.contains(event.target) && !sortMenu.contains(event.target)) {
+    sortMenu.classList.remove("active");
+  }
 });
